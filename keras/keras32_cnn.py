60ad2f4d605b4e73ea688239def65c4d67f81589
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-01-13 17:07:13 +0900

    23-01-13

--- keras/keras32_cnn.py
+++ keras/keras32_cnn.py
@@ -1,19 +1,46 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten   #Conv2D는 2차원
 
-model = Sequential()
 
+
+model = Sequential()
+                            # input = (60000, 5, 5, 1)
 model.add(Conv2D(filters=10, kernel_size=(2,2),          #filter는 안써도 상관 없음. kernel_size는 5,5짜리의 그림을 2,2크기의 그림으로 잘라서 4,4짜리의 그림이 됨.
-                 input_shape=(5, 5, 1)))                #filter는 4,4크기의 그림 하나가 10개 들어간다. 
-model.add(Conv2D(filters=5, kernel_size=(2,2)))          #Dense모양과 연결되어야함.
-model.add(Flatten())                                    #flatten전의 데이터들은 전부 펴짐
-model.add(Dense(10))
-model.add(Dense(1))
+                 input_shape=(5, 5, 1)))             #(N,4,4, 10) 60000개를 N으로 써도 상관없음       #filter는 4,4크기의 그림 하나가 10개 들어간다. 
+                                                     # (batch_size, row. columns, channels) channels는 color batch_size는 훈련횟수로 연산. 
+
+
+
+model.add(Conv2D(filters=5, kernel_size=(2,2)))      #(N,3,3,5)            #Dense모양과 연결되어야함.
+model.add(Flatten())                                 #(N, 45)       #flatten전의 데이터들은 전부 펴짐
+model.add(Dense(units=10))                           #(N, 10)       Conv2D에서 Dense의 기본 데이터는 units로 되어있다.
+        #input 은 (batch_size, input_dim)
+model.add(Dense(4, activation='relu'))  # 지현, 성환, 건률, 렐루                                  #(N, 1)
 
 model.summary()
 
 
 
+'''
+    padding -   valid : 유효한 영역만 출력. 출력 이미지는 사이즈 입력 사이즈보다 작다.
+                same : 출력 이미지 사이즈가 입력 이미지 사이즈와 동일.
+
+    kernel_size : 2D 컴볼루션 창으 ㅣ높이와 너비를 지정하는 정수 또는 2개 정수의 튜플/목록
+
+
+    input_shape : 샘플 수를 제외한 입력 형태를 정의. 모델에서 첫 레이어일 때만 정의하면 된다. 
+                 (행, 열, 채널 수)로 정의. 흑백영상인 경우 채널이 1, 컬러인 경우 채널은 3으로 설정.
+
+    activation  - linear : 디폴트 값, 입력뉴런과 가중치로 계산된 결과값이 그대로 출력으로 나온다.
+                - relu : rectifier 함수, 은익층에 주로 쓰인다.
+                - sigmoid : 시그모이드 함수, 이진 분류 문제에서 출력층에 주로 쓰인다.
+                - softmax : 소프트맥스 함수, 다중 클래스 분류 문제에서 출력층에 주로 쓰인다.
+
+    strides :   기본적으로 입력 데이터와 필터를 연산할 때는 한 칸씩 이동하면서 연산을 하는데 스트라이드는 입력데이터에 필터를 적용할 때 이동할 간격을 조절해주는 파라미터.
+    
+    
+    
+    output_shape : 
 
 
 
@@ -22,7 +49,7 @@ model.summary()
 
 
 
+5,5,1
 
 
-
-
+'''
---
+++ keras/keras34_2_cifar10.py
@@ -0,0 +1,64 @@
+from tensorflow.keras.datasets import cifar10, cifar100
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import datetime
+#컬러 데이터
+
+
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+print(x_train.shape, y_train.shape) # (50000, 32, 32, 3) (50000, 1)
+print(x_test.shape, y_test.shape)   #(10000, 32, 32, 3) (10000, 1)
+
+print(np.unique(y_train, return_counts=True))   
+# (array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000],dtype=int64))
+
+#2. 모델구성
+model = Sequential()
+model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32, 32, 3), activation='relu'))    # (27, 27, 128)
+model.add(Conv2D(filters=64, kernel_size=(2,2)))    # (26, 26, 64)
+model.add(Conv2D(filters=64, kernel_size=(2,2)))    # (25, 25, 64)  flattne -> 40000
+model.add(Conv2D(filters=32, kernel_size=(2,2)))    # (25, 25, 64)  flattne -> 40000
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))             #input_shape = (40000)
+                                                    # (60000, 40000)    (batch_size, input_dim)
+model.add(Dense(10, activation='softmax'))
+
+#3. 컴파일, 훈련
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min',
+                              restore_best_weights=True,                        
+                              verbose=1 
+                              )
+
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")
+
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'        #epoch:04는 숫자 네자리까지  ex) 37번의 값이 제일 좋으면 0037 val_loss는 소수점 4번째 자리까지 나오게 됨.
+ 
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+                      save_best_only=True,
+                    #   filepath = path +'MCP/keras30_ModelCheckPoint3.hdf5'
+                      filepath = filepath + 'k34_02_' + date + '_' + filename
+                      )
+
+
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
+              metrics=['acc'])  #metrics에 accuracy가 들어갔기 때문에 loss와 accuracy값이 나옴.
+model.fit(x_train, y_train, epochs=100, batch_size=32,
+          callbacks=[es, mcp],
+          verbose=1,
+          validation_split=0.2,
+          ) 
+
+
+#4. 평가, 예측
+result = model.evaluate(x_test, y_test)
+print('loss : ', result[0])
+print('acc : ', result[1])
