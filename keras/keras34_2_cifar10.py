739f8dae4350f26ea37497fdf6a65a92a08cb606
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-01-13 18:36:01 +0900

    23-01-13

--- keras/keras34_2_cifar10.py
+++ keras/keras34_2_cifar10.py
@@ -72,6 +72,20 @@ print('acc : ', result[1])
 loss :  1.204951524734497
 acc :  0.5916000008583069
 
+
+
+model = Sequential()
+model.add(Conv2D(filters=200, kernel_size=(3,3), input_shape=(32, 32, 3), activation='relu'))    # (31, 31, 128)
+model.add(MaxPooling2D((2, 2))) 
+model.add(Conv2D(filters=100, kernel_size=(3,3)))    # (30, 30, 64)  
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(filters=70, kernel_size=(2,2)))    # (28, 28, 32)  flatten -> 25088
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))             #input_shape = (40000)
+                                                    # (60000, 40000)    (batch_size, input_dim)
+                                                    
+epochs=100, batch_size=32,
+model.add(Dense(10, activation='softmax'))
 loss :  0.9594894647598267
 acc :  0.6726999878883362
     
