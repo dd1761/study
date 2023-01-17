d9d425ddefd53f462421ee72bb9c8fd370fdfc4c
dd1761 <97274814+dd1761@users.noreply.github.com>
2023-01-17 21:30:08 +0900

    23-01-17

--- keras/train.py
+++ keras/train.py
@@ -17,5 +17,25 @@ print(x_test.shape, y_test.shape)
 print(np.unique(y_train, return_counts=True))
 
 model = Sequential()
+model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32, 32, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(filters=64, kernel_size=(2,2)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(filters=32, kernel_size=(2,2)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(100, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+es = EarlyStopping(monitor='val_loss', patience=10, mode='auto')
+modelpath = '../data/modelcheckpoint/k45_cifar100_{epoch:02d}-{val_loss:.4f}.hdf5'
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
+start_time = datetime.datetime.now()
+model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=1, callbacks=[es, cp])
+end_time = datetime.datetime.now()
+time = end_time - start_time
+print("걸린시간 : ", time)
 
 
