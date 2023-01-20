c08357117e219b71d82e2c7a58df2968b36fd56e
dd1761 <97274814+dd1761@users.noreply.github.com>
2023-01-20 21:54:56 +0900

    23-01-20

--- keras/keras34_3_cifar100.py
+++ keras/keras34_3_cifar100.py
@@ -48,7 +48,7 @@ model.add(Dropout(0.1))
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(filters=44, kernel_size=(2,2)))    # (28, 28, 32)  flatten -> 25088
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))             #input_shape = (40000)
+model.add(Dense(24, activation='relu'))             #input_shape = (40000)
                                                     # (60000, 40000)    (batch_size, input_dim)
 model.add(Dense(100, activation='softmax'))     
 
