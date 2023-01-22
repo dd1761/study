5a0b707f4abe100b86536f2fa38cd2818a0f88aa
dd1761 <97274814+dd1761@users.noreply.github.com>
2023-01-22 22:33:53 +0900

    23-01-22

--- keras/keras34_3_cifar100.py
+++ keras/keras34_3_cifar100.py
@@ -68,7 +68,7 @@ filename = '{epoch:04d}-{val_loss:.4f}.hdf5'        #epoch:04는 숫자 네자�
 mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
                       save_best_only=True,
                     #   filepath = path +'MCP/keras30_ModelCheckPoint3.hdf5'
-                      filepath = filepath + 'k34_03_' + date + '_' + filename
+                      filepath = filepath + 'k34_03_' + date + '_' + filename   #k34_03_1015_1530_0001-1.0000.hdf5
                       )
 
 
