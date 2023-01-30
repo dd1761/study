3c650c2212f06d2e38baee2c59a94fc59139d778
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-01-30 17:43:22 +0900

    23-01-30

--- keras/keras53_1_ImageDataGenerator.py
+++ keras/keras53_1_ImageDataGenerator.py
@@ -30,13 +30,42 @@ xy_train = train_datagen.flow_from_directory(
     class_mode='binary',                # 수치형으로 변환
     color_mode='grayscale',             # 흑백으로 변환
     shuffle=True,                       # 데이터를 섞어준다. 파이썬에서는 함수(괄호)안에서 ,를 마지막에 찍어도 작동이 된다.    
+    # Found 160 images belonging to 2 classes.
 )
 
-# Found 160 images belonging to 2 classes.
+xy_test = test_datagen.flow_from_directory(
+    './_data/brain/test/',             # 폴더 경로 지정
+    target_size=(200, 200),             # 이미지 사이즈 지정
+    batch_size=10,                       
+    class_mode='binary',                # 수치형으로 변환
+    color_mode='grayscale',             # 흑백으로 변환
+    shuffle=True,                       # 데이터를 섞어준다. 파이썬에서는 함수(괄호)안에서 ,를 마지막에 찍어도 작동이 된다.    
+    # Found 120 images belonging to 2 classes.
+)
 
-# test_generator = test_datagen.flow_from_directory(
-#     '../data/image/brain/test',
-#     target_size=(150, 150),
-#     batch_size=5,
-#     class_mode='binary'
-# )
+print(xy_train)
+# <keras.preprocessing.image.DirectoryIterator object at 0x0000018B20D77AC0>
+
+from sklearn.datasets import load_iris
+datasets = load_iris()
+print(datasets)
+
+
+# print(xy_train[0])
+# print(xy_train[0][0])
+print(xy_train[0][0].shape)         # (7, 200, 200, 1)
+print(xy_train[0][1])               # y값 출력  [0. 0. 1. 1. 1.]
+print(xy_train[0][1].shape)         # (7,)
+ 
+print(type(xy_train))                # <class 'keras.preprocessing.image.DirectoryIterator'>
+print(type(xy_train[0]))             # <class 'tuple'>  리스트와 동일하다. 튜플은  한번 생성하면 수정이 불가능하다.
+print(type(xy_train[0][0]))          # <class 'numpy.ndarray'> numpy로 변경
+print(type(xy_train[0][1]))          # <class 'numpy.ndarray'> numpy로 변경
+
+
+
+
+
+
+
+
